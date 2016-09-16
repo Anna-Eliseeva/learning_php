1c6a3e4b48de2225d6b493101bafa2300f7bf3d0
Anna-Eliseeva <eliseeva9797@mail.ru>
2016-09-16 19:56:25 +0300

    calculator

--- trim.php
+++ trim.php
@@ -1,6 +1,17 @@
 <?php
-$text = ' Anna Anna ';
-var_dump($text);
-echo "\n \n";
-$text = trim($text);
-var_dump($text);
+$a = $_REQUEST["a"];
+$b = $_REQUEST["b"];
+$radio = $_REQUEST["operator"];
+
+var_dump($_REQUEST);
+switch ($radio) {
+    case '+':
+        echo $a + $b;
+    break;
+    case '-':
+        echo $a - $b;
+    break;
+    case '/':
+        echo $a / $b;
+    break;
+}
